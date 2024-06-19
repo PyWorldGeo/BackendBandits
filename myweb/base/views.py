200f30f8751c0e4d3c1c19ca77d8c4f40dd34616
12DoRoRo12 <n.tsitsqishvili@gmail.com>
2024-06-19 20:49:35 +0400

    profile

--- myweb/base/views.py
+++ myweb/base/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Book, User
 
 # Create your views here.
 def home(request):
-    context = {"word": "Hello World!!!"}
-    return render(request, 'home.html', context)
+    books = Book.objects.all()
+    context = {"books": books}
+    return render(request, 'base/home.html', context)
 
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'base/about.html')
+
+
+def profile(request, pk):
+    user = User.objects.get(id=pk)
+    books = user.books.all()
+    context = {"books": books}
+    return render(request, 'base/profile.html', context)
