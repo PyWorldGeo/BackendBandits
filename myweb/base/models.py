5178c5275bc3bca4822defd8a73d198d7b6d65f1
12DoRoRo12 <n.tsitsqishvili@gmail.com>
2024-06-24 20:41:50 +0400

    Search

--- myweb/base/models.py
+++ myweb/base/models.py
@@ -11,10 +11,18 @@ class Author(models.Model):
         return self.name
 
 
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
 class Book(models.Model):
     picture = models.CharField(max_length=300)
     name = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.SET("Unknown Author"))
+    genre = models.ManyToManyField(Genre, related_name='books', blank=True)
     description = models.TextField(max_length=500)
     content = models.CharField(max_length=200)
 
@@ -23,4 +31,4 @@ class Book(models.Model):
 
 
 class User(AbstractUser):
-    books = models.ManyToManyField(Book, related_name='books', blank=True)
+    books = models.ManyToManyField(Book, related_name='users', blank=True)
--- myweb/base/views.py
+++ myweb/base/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Book, User
+from .models import Book, User, Genre
+from django.db.models import Q
+
 
 # Create your views here.
 def home(request):
-    books = Book.objects.all()
-    context = {"books": books}
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    books = Book.objects.filter(Q(name__icontains=q) | Q(description__icontains=q) | Q(author__name__icontains=q) | Q(genre__name__icontains=q))
+    books = list(set(books))
+    # books = Book.objects.all()
+    # print(books[2].users.all())
+    heading = "Online Library"
+    genres = Genre.objects.all()
+    context = {"books": books, "heading": heading, 'genres': genres}
     return render(request, 'base/home.html', context)
 
 
@@ -15,6 +23,11 @@ def about(request):
 
 def profile(request, pk):
     user = User.objects.get(id=pk)
-    books = user.books.all()
-    context = {"books": books}
+    # books = user.books.all()
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    books = user.books.filter(Q(name__icontains=q) | Q(description__icontains=q) | Q(author__name__icontains=q) | Q(genre__name__icontains=q))
+    books = list(set(books))
+    heading = "My Library"
+    genres = Genre.objects.all()
+    context = {"books": books, "heading": heading, 'genres': genres}
     return render(request, 'base/profile.html', context)
