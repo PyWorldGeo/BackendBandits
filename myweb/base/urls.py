ad88a86c0626194d2bdd4775da8ad41a2c8a496d
12DoRoRo12 <n.tsitsqishvili@gmail.com>
2024-07-01 20:44:12 +0400

    login

--- myweb/base/urls.py
+++ myweb/base/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('profile/<str:pk>/', views.profile, name='profile')
+    path('profile/<str:pk>/', views.profile, name='profile'),
+    path('adding/<str:id>/', views.adding, name='adding'),
+    path('delete/<str:id>/', views.delete, name='delete'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout')
 ]
--- myweb/base/views.py
+++ myweb/base/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Book, User, Genre
 from django.db.models import Q
-
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
@@ -17,10 +18,11 @@ def home(request):
     return render(request, 'base/home.html', context)
 
 
+
 def about(request):
     return render(request, 'base/about.html')
 
-
+@login_required(login_url='login')
 def profile(request, pk):
     user = User.objects.get(id=pk)
     # books = user.books.all()
@@ -31,3 +33,51 @@ def profile(request, pk):
     genres = Genre.objects.all()
     context = {"books": books, "heading": heading, 'genres': genres}
     return render(request, 'base/profile.html', context)
+
+
+@login_required(login_url='login')
+def adding(request, id):
+    user = request.user
+    book = Book.objects.get(id=id)
+    user.books.add(book)
+    return redirect('profile', user.id)
+
+
+@login_required(login_url='login')
+def delete(request, id):
+    obj = Book.objects.get(id=id)
+
+    if request.method == "POST":
+        request.user.books.remove(obj)
+        return redirect('profile', request.user.id)
+
+    return render(request, 'base/delete.html', {'obj': obj})
+
+
+
+def login_user(request):
+    if request.user.is_authenticated:
+        return redirect('profile', request.user.id)
+
+    if request.method == 'POST':
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            pass
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('profile', request.user.id)
+        else:
+            pass
+
+    return render(request, 'base/login.html')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
